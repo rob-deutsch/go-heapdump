689ce0a64ccaeaf149f1b452cb802b2c796ecdb6
rob-deutsch <robzyb+altgithub@gmail.com>
2018-09-29 19:28:35 +1000

    Using go pipes to count characters

--- main.go
+++ main.go
@@ -1,17 +1,48 @@
 package main
 
 import (
-	"os"
+	"fmt"
+	"io/ioutil"
 	"os/exec"
 )
 
 func main() {
-	//cmd := exec.Command("wc", "-b")
-	cmd := exec.Command("ls", "-la")
-	cmd.Stdout = os.Stdout
-	err := cmd.Run()
+	// Setup the command tobe run
+	cmd := exec.Command("wc", "-c")
+
+	// Get stdin and stdout
+	stdin, err := cmd.StdinPipe()
 	if err != nil {
 		panic(err)
 	}
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+
+	// Start the process
+	if err := cmd.Start(); err != nil {
+		panic(err)
+	}
+
+	// Write the data
+	if _, err := stdin.Write([]byte("abcracadabra")); err != nil {
+		panic(err)
+	}
+	stdin.Close()
+
+	// Get the result
+	result, err := ioutil.ReadAll(stdout)
+	if err != nil {
+		panic(err)
+	}
+
+	// Wait for the command to exit
+	// if err = cmd.Wait(); err != nil {
+	// 	panic(err)
+	// }
+
+	// Print the result
+	fmt.Println(string(result))
 	return
 }
